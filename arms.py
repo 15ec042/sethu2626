0f1c817072916a720e41586ead417db5236bedb5
15ec042 <sethuanand619@gmail.com>
2018-03-27 15:56:49 +0530

    Create arms.py

---
+++ arms.py
@@ -0,0 +1,13 @@
+x=int(raw_input())
+y=int(raw_input())
+ 
+for n in range(a,b):
+   a = 0
+   b= n
+   while b > 0:
+       digit = b % 10
+       a=a+ digit ** 3
+       temp /= 10
+ 
+   if n == sum:
+print(n)
