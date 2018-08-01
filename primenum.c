2496c4c4d20ddbf7ff3b4433a46e7e841e3fcea7
15ec042 <sethuanand619@gmail.com>
2018-08-01 16:16:38 +0530

    Create primenum.c

---
+++ primenum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a>=0)
+{
+    if(a/a=1)
+    {
+        printf("Prime Number");
+    }
+}
+else
+{
+    printf("Not Prime Number")
+}
+}
+}                                     
+}
