d7032f5fcdeb60dc1aa0de7afedc0d3c3306dfc7
15ec042 <sethuanand619@gmail.com>
2018-08-06 15:49:27 +0530

    Update mediaan.py

--- mediaan.py
+++ mediaan.py
@@ -1,12 +1,12 @@
-n=input("enter n value\n")
+na=input("enter n value\n")
 sdd=[]
-for i in range(0,n):
-	b=int(input())
-	sdd.append(b)
+for i in range(0,na):
+	ba=int(input())
+	sdd.append(ba)
 sdd.sort()
-if(n%2 != 0):
-	print(sdd[(n/2)])
+if(na%2 != 0):
+	print(sdd[(na/2)])
 else:
-	s=n/2
+	s=na/2
 	c=(int(sdd[s])+int(sdd[s-1]))/2
 	print(c)
