79c51c7f58cbb1196b45bd598b3cc1584dc80dbd
15ec042 <sethuanand619@gmail.com>
2018-08-06 15:32:47 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,8 +1,8 @@
-number=int(input("enter the length:"))
-list=[]
-for i in range(0,number):
+numbe=int(input("enter the length:"))
+lists=[]
+for i in range(0,numbe):
     newNom=int(input(''))
-    aa=list.append(newNom)
-    print list
-list.sort()
-print("Sorted list is",list)
+    aa=lists.append(newNom)
+    print lists
+lists.sort()
+print("Sorted list is",lists)
