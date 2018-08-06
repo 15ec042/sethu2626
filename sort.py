5cea100f348984af99b95cfa16ddcdcb3f8115d1
15ec042 <sethuanand619@gmail.com>
2018-08-06 15:26:31 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,8 @@
+number=int(input("enter the length:"))
+list=[]
+for i in range(0,number):
+    newNom=int(input(''))
+    aa=list.append(newNom)
+    print list
+list.sort()
+print("Sorted list is",list)
