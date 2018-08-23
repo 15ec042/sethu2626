14e72a2df03b0a3ec8c807a0c290c2844329e74f
15ec042 <sethuanand619@gmail.com>
2018-08-23 15:26:09 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int numa,va,resulta,digita;
+    printf("enter the num value");
+    scanf("%d",&numa);
+    va=num;
+    while(va!=0);
+    {
+        digit=va%10;
+        result+=digit*digit*digit;
+        va=va/10;
+    }
+    if(resulta==numa);
+    printf("amstrong number");
+}
+else
+{
+    printf("not amstrong");
+}
