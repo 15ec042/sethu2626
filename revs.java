e7eb851cf0000f0d5b9cd7ce15ac1fcfc0b92306
15ec042 <sethuanand619@gmail.com>
2018-08-23 14:41:57 +0530

    Create revs.java

---
+++ revs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+public class dinesh {
+     public static void main(String args[]){
+     Scanner in=new Scanner(System.in);
+      String ss=in.nextLine(),ss1; ss1=ss.replaceAll("[aeiouAEIOU]","");
+      StringBuilder ss=new StringBuilder();
+      sss.append(s1);
+     for(int i=sss.length()-1;i>=0;i--)
+     {
+               System.out.print(ss
+               ss.charAt(i));
+}}}
