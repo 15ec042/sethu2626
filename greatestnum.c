313c3d8d1392d9bd659cc327d6c696214d7d7cf4
15ec042 <sethuanand619@gmail.com>
2019-02-06 09:53:43 +0530

    Create greatestnum.c

---
+++ greatestnum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,y,m,n;
+	scanf("%d %d",&x,&y);
+	for(i=2;i<=x;i++)
+	{
+		if(x%i==0 && y%i==0)
+		{
+			m=n;
+		}
+	}
+	printf("%d",m);
+	return 0;
+}
